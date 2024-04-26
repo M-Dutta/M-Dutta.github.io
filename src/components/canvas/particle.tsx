e0db0bd29c50db3ff0c51d24968ca9a124713e7e
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2024-04-26 16:10:36 -0500

    Fixing z-indexes and limiting FPS (#5)
    * Fixed Particle z-indexing
    * Fixed fps

--- src/components/canvas/particle.tsx
+++ src/components/canvas/particle.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { Box } from "@material-ui/core";
 
 const particleOptions = require('./particlesjs-config.json');
 const BGParticles = () => {
@@ -14,16 +15,13 @@ const BGParticles = () => {
 			setInit(true);
 		});
 	}, []);
-
-	const particlesLoaded = (container) => {
-		console.log(container);
-	};
-
+	// Particles component isn't respecting "zIndex" from the .json file. Box around it and force the z-index
 	return (
-		init && <Particles id="tsparticles"
-			particlesLoaded={particlesLoaded}
-			options={particleOptions}
-		/>
+		init &&
+		<Box style={{ position: 'fixed', zIndex: -1 }}>
+			<Particles id="tsparticles" options={particleOptions} />
+		</Box>
+
 	)
 }
 
